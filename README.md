84c185474e749c4597a4f3e3972ff8e6aeb50cfd
Matthew Shafransky <matt@Matthews-MacBook-Pro.local>
2021-09-01 12:59:00 -0400

    code added, bugs fixed

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Book-Search-Engine
+# MERN: Book Search Engine
+
+![header](./images/banner.jpg)
+
+## 📝 Description
+
+```
+The Book Search Engine app allows you to search for books usin the Google
+API.
+
+You can create a login and save a link and description of books you like to
+your profile.
+
+This is a functioning application that used a RESTful backend for it's API
+searching. It has been modified to use GraphQL as an alternative to the 
+RESTful API routes.
+```
+
+## 🧰 Technologies Used
+
+- `Express Node.js`
+- `npm (Node Package Manager)`
+- `Javascript`
+- `Apollo Server`
+- `Bootstrap`
+- `GraphQL`
+- `Bcrypt`
+- `Mongoose`
+- `MongoDB Atlas`
+- `REACT`
+
+## 📄 Installation Instructions
+
+You will need to run npm install from the root directory as well as from the 
+server and client folders to install all required packages. 
+
+
+
+## 🔗 Project Links
+
+[Heroku Deployment])
+)
+
+## 📱 Contact
+
+Matthew Shafransky - [Github](https://github.com/MpShafransky) 
+
---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+
+// Setup to use apollo server on the back-end.
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+let server;
+async function startApolloServer() {
+  server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  playground: true
+});
+
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+startApolloServer()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+}
+
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
